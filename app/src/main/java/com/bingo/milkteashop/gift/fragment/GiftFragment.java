27fcd42e7664da07b1112f12a3b40ef0a4677ea6
BingoBinBingo <huangbin.tx@star-net.com>
2016-07-09 16:18:07 +0800

    ADD:添加GiftFragment页面

---
+++ app/src/main/java/com/bingo/milkteashop/gift/fragment/GiftFragment.java
@@ -0,0 +1,52 @@
+package com.bingo.milkteashop.gift.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bingo.milkteashop.BaseFragment;
+import com.bingo.milkteashop.R;
+
+/**
+ * Created by Bingo on 16/7/8.
+ */
+public class GiftFragment extends BaseFragment {
+    private static final String TAG = GiftFragment.class.getCanonicalName();
+
+    public static GiftFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        GiftFragment fragment = new GiftFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    protected void initModel() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected View initView(LayoutInflater inflater, @Nullable ViewGroup container) {
+        View rootView = inflater.inflate(R.layout.gift_fragment_layout, container, false);
+        return rootView;
+    }
+
+    @Override
+    protected void renderView() {
+
+    }
+}
